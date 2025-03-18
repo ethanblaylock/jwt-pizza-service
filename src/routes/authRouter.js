69ce5f0ac3bc14ebbbf4f60031a0b192bc90b455
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2025-03-18 13:15:00 -0600

    fixed file path issue

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const config = require('../config.js');
-const metrics = require('./metrics');
+const metrics = require('../metrics.js');
 const { asyncHandler } = require('../endpointHelper.js');
 const { DB, Role } = require('../database/database.js');
 
